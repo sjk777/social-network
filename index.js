5311348653a2ce8d5affc9200649e967ffb34183
Sky K <skyj.kim7@gmail.com>
2023-02-26 20:12:36 -0800

    added app.use and db channel

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ const app = express();
 const activity = cwd.includes('social-network')
 ? cwd.split('/social-network/')[1] : cwd;
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(routes);
+
+db.once('open',()=>{
+    app.listen(PORT, ()=>{
+        console.,log(`API server for ${activity} running on port ${PORT}!`);
+    });
+});
